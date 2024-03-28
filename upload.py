cd7f03538a41d36a84e608b26cdd0a3f06173c04
mayi <law52526@gmail.com>
2024-03-28 10:34:20 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import logging
 from dotenv import load_dotenv
 from logging.handlers import TimedRotatingFileHandler
@@ -15,8 +16,7 @@ if __name__ == "__main__":
 
     # 创建一个TimedRotatingFileHandler并设置level为info
     current_dir_path = os.path.dirname(os.path.realpath(__file__))
-    if not os.path.exists(os.path.join(current_dir_path, 'logs')):
-        os.mkdir(os.path.join(current_dir_path, 'logs'))
+    # handler = TimedRotatingFileHandler(os.path.join(current_dir_path, 'logs', 'log.txt'), when='midnight', interval=1, backupCount=30)
     handler = TimedRotatingFileHandler(os.path.join(current_dir_path, 'logs', 'log.txt'), when='midnight', interval=1, backupCount=30)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -25,4 +25,13 @@ if __name__ == "__main__":
     RedisManager.init_config(redis_host=os.getenv("REDIS_HOST"), redis_port=os.getenv("REDIS_PORT"), password=None)
 
     pan = BaiDuPanHelper(app_id=os.getenv("APPID"), secret=os.getenv("SECRET"), app_name=os.getenv("APPNAME"), logger=logger)
-    print(pan.upload_file(file_path=os.path.join(current_dir_path, 'backup', 'uploadsdk.tar.gz')))
+    # 创建参数解析对象
+    parser = argparse.ArgumentParser(description="处理命令行文件路径参数")
+
+    # 添加我们想要解析的参数，这里是路径参数
+    parser.add_argument("filepath", help="文件路径参数")
+
+    # 使用我们的参数解析对象解析参数
+    args = parser.parse_args()
+
+    pan.upload_file(file_path=args.filepath)
